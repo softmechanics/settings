09b8bc4d12c3771172d3a0e1dc7b9cf304670196
Matt Brown <matt@softmechanics.net>
2010-08-14 17:42:15 -0700

    removed unnecessary Settings class variants

--- Data/Settings.hs
+++ Data/Settings.hs
@@ -2,16 +2,5 @@
 
 module Data.Settings where
 
--- | Simple lookup indexed by type.
-class Settings a where
-  settings :: a
-  
--- | Key-value lookup.  Useful when there are multiple dependencies on
--- a single type.
-class MapSettings a b | a -> b where
-  mSettings :: a -> b
-
--- | Key-value lookup, with multiple values for a single key.
-class MultiMapSettings a b where
-  mmSettings :: a -> b
-
+class Settings a b | a -> b where
+  settings :: a -> b
